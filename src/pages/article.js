1224971e6629042d36ae46a267e8c797d79ba372
justine ha <90005137+justineha@users.noreply.github.com>
2022-09-29 09:33:57 -0400

    finalizing article.js

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,28 +1,48 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { useParams } from "react-router";
-import Data from '../components/data';
+import Data from "../components/data";
 
 function Article() {
     const {id} = useParams();
-
     const articleData = Data.find((article) => article.id === id);
 
+    const articleDate = useMemo(() => {
+        if (!articleData) return "";
+        const parsedDate = new Date(articleData.publishedDate)
+        return parsedDate.toDateString();
+     }, [articleData]);
+
     return (
-        <main>
-            <header>
-                <div className="headercontent">
-                    <h1>{articleData.title}</h1>
-                    <p className="title-date">Tuesday, August 22, 2022</p>
-                    <p className="title-blurb">Note that this is an Article Blurb. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
+        <main className="article">
+            <header
+                className="headercontent"
+                style={{
+                    backgroundImage: `url('${articleData.image.url}')`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                }}
+            >     
+                <h1>{articleData.title}</h1>
+                <p>{articleDate}</p>
+                <p className="title-blurb">{articleData.blurb}</p>
             </header>
             <section>
                 <div className="sectioncontent">
-                    <p className="section-blurb">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate odio ut enim blandit volutpat maecenas volutpat. Mattis molestie a iaculis at erat pellentesque adipiscing. In eu mi bibendum neque egestas congue quisque. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Euismod elementum nisi quis eleifend quam adipiscing. Quis hendrerit dolor magna eget est. Leo urna molestie at elementum eu facilisis sed. Nisi scelerisque eu ultrices vitae. Sem integer vitae justo eget magna fermentum iaculis eu non. Varius sit amet mattis vulputate enim nulla. Quis commodo odio aenean sed adipiscing. Felis bibendum ut tristique et egestas quis ipsum suspendisse.</p>
-                    <h2>Header Two Inserted</h2>
-                    <p className="section-paragraph1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate odio ut enim blandit volutpat maecenas volutpat. Mattis molestie a iaculis at erat pellentesque adipiscing. In eu mi bibendum neque egestas congue quisque. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Euismod elementum nisi quis eleifend quam adipiscing. Quis hendrerit dolor magna eget est. Leo urna molestie at elementum eu facilisis sed. Nisi scelerisque eu ultrices vitae. Sem integer vitae justo eget magna fermentum iaculis eu non. Varius sit amet mattis vulputate enim nulla. Quis commodo odio aenean sed adipiscing. Felis bibendum ut tristique et egestas quis ipsum suspendisse.</p>
-                    <h3>Header Three Inserted</h3>
-                    <p className="section-paragraph2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vulputate odio ut enim blandit volutpat maecenas volutpat. Mattis molestie a iaculis at erat pellentesque adipiscing. In eu mi bibendum neque egestas congue quisque. Tristique sollicitudin nibh sit amet commodo nulla facilisi. Cursus risus at ultrices mi tempus imperdiet nulla malesuada. Euismod elementum nisi quis eleifend quam adipiscing. Quis hendrerit dolor magna eget est. Leo urna molestie at elementum eu facilisis sed. Nisi scelerisque eu ultrices vitae. Sem integer vitae justo eget magna fermentum iaculis eu non. Varius sit amet mattis vulputate enim nulla. Quis commodo odio aenean sed adipiscing. Felis bibendum ut tristique et egestas quis ipsum suspendisse.</p>
+                    {articleData.articleText.map((text, i) => {
+                        const type = text.type;
+                        switch(type) {
+                           case "p":
+                                return <p key={i}>{text.data}</p>; /* every single element in react needs a unique identifier*/
+                            case "h1":
+                                return <p key={i}>{text.data}</p>;
+                            case "h2":
+                                return <p key={i}>{text.data}</p>;
+                            case "h3":
+                                return <p key={i}>{text.data}</p>;
+                            default:
+                                return <p key={i}>{text.data}</p>;
+                        }
+                    })}
                 </div>
             </section>
         </main>
